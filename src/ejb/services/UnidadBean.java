5d2b9c1ecf499d6a3d7ad372f7b1807a32c9951e
Fla-dani-mathi\Dani-Fla-Mathi <danielnacher@gmail.com>
2017-02-03 00:01:35 -0300

    se crean algunas funcionalidades de gastos comunes

--- src/ejb/services/UnidadBean.java
+++ src/ejb/services/UnidadBean.java
@@ -3,6 +3,7 @@ package ejb.services;
 import entities.hibernate.SessionConnection;
 import entities.persistence.entities.Unidad;
 import exceptions.ServiceException;
+import static java.lang.Math.toIntExact;
 import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -89,5 +90,23 @@ public class UnidadBean implements UnidadLocal{
         session.close();
         return unidades;
     }
+    
+    public int totalUnidades(String block, int torre){
+        Session session = SessionConnection.getConnection().useSession();
+        if(block.equals("") && torre==0){        
+            Query query = session.createQuery("select count(*) from Unidad unidad");
+            Long count = (Long)query.uniqueResult();       
+            int retorno=toIntExact(count);
+            return retorno;
+        }
+        else{
+            Query query = session.createQuery("select count(*) from Unidad unidad where unidad.block=:elBlock and unidad.torre=:laTorre");
+            query.setParameter("elBlock", block);
+            query.setParameter("laTorre", torre);
+            Long count = (Long)query.uniqueResult();       
+            int retorno=toIntExact(count);
+            return retorno;
+         }
+     }
     
 }
--- src/web/controller/GastosComunesController.java
+++ src/web/controller/GastosComunesController.java
@@ -1,11 +1,20 @@
 package web.controller;
 
+import ejb.services.UnidadBean;
+import entities.persistence.entities.Unidad;
+import exceptions.ServiceException;
+import java.math.RoundingMode;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.PieChart;
@@ -14,8 +23,11 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import web.animations.FadeInUpTransition;
 
@@ -29,7 +41,7 @@ public class GastosComunesController implements Initializable {
    
 
     @FXML
-    private TableView<?> tableGastosComunes;
+    private TableView<Unidad> tableGastosComunes;
 
     @FXML
     private DatePicker cmbFecha;
@@ -44,7 +56,7 @@ public class GastosComunesController implements Initializable {
     private ProgressBar bar;     
 
     @FXML
-    private ComboBox<?> cmbTorre;
+    private ComboBox<Integer> cmbTorre;
 
     @FXML
     private AnchorPane paneGastosComunes;
@@ -53,17 +65,29 @@ public class GastosComunesController implements Initializable {
     private CheckBox chkBonificacion;
 
     @FXML
-    private ComboBox<?> cmbBlock;
+    private ComboBox<String> cmbBlock;
     
     @FXML
     private Button btnMostrar;
     
+    ObservableList<Unidad> retorno;
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        atras();
-        btnMostrar.setDisable(true);   
+        UnidadBean ub=new UnidadBean();
+        try {
+            retorno=FXCollections.observableArrayList(ub.traerTodos());
+        } catch (ServiceException ex) {
+            Logger.getLogger(GastosComunesController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        atras();         
         cargaHoy();
-    }     
+        cargarComboBlock();
+        cargarComboTorre();
+        cargaTabla();
+        //cargaGrafica(cmbBlock.getSelectionModel().getSelectedItem(), cmbTorre.getSelectionModel().getSelectedItem());
+    }
     
     public void cargaHoy(){
         Date date= new Date();
@@ -96,4 +120,70 @@ public class GastosComunesController implements Initializable {
      private void auto(){
       
     }
-} 
+     
+     public void cargarComboBlock(){
+       ObservableList<String> options = 
+    FXCollections.observableArrayList("A","B","C","D","E");
+       cmbBlock.setItems(options);
+    }
+    
+    public void cargarComboTorre(){
+        ObservableList<Integer> listaTorres;
+        listaTorres=FXCollections.observableArrayList(1,2,3,4,5,6);
+        cmbTorre.setItems(listaTorres);
+    }
+    
+    public void cargaTabla(){
+       TableColumn Nombre = new TableColumn("Nombre");
+       TableColumn Apellido = new TableColumn("Apellido");
+       TableColumn Block = new TableColumn("Block");
+       TableColumn Torre = new TableColumn("Torre");
+       TableColumn Puerta= new TableColumn("Puerta");
+
+       Nombre.setMinWidth(100);
+       Nombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+
+       Apellido.setMinWidth(100);
+       Apellido.setCellValueFactory(new PropertyValueFactory<>("apellido"));
+
+       Block.setMinWidth(50);
+       Block.setCellValueFactory(new PropertyValueFactory<>("Block"));
+
+       Torre.setMinWidth(50);
+       Torre.setCellValueFactory(new PropertyValueFactory<>("Torre"));
+
+       Puerta.setMinWidth(60);
+       Puerta.setCellValueFactory(new PropertyValueFactory<>("Puerta"));
+
+       tableGastosComunes.getColumns().addAll(Nombre, Apellido, Block,Torre,Puerta);
+       tableGastosComunes.setItems(retorno);
+       cargaGrafica("",0);
+    }
+    
+    public void cargaGrafica(String block, int torre){
+        //LblInfoPieChart.setText("");
+        UnidadBean ub=new UnidadBean();
+        int total=ub.totalUnidades(block,torre);
+        int totalPago=total-retorno.size();
+        int totalNoPago=total-totalPago;
+        ObservableList<PieChart.Data> lista=FXCollections.observableArrayList(                
+                new PieChart.Data("No pagó", totalNoPago),
+                new PieChart.Data("Pagó", totalPago)
+        );
+        chartGastosComunes.setData(lista);
+        
+        for(final PieChart.Data data: chartGastosComunes.getData()){
+            data.getNode().addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
+                int num=(int)data.getPieValue();
+                double porc=(((double)num*(double)100)/total);
+                DecimalFormat df = new DecimalFormat("#.#");
+                df.setRoundingMode(RoundingMode.CEILING);
+                String porcent=df.format(porc);
+                //LblInfoPieChart.setText(data.getName()+ ": " + num + " (" + porcent + " % aprox.)");
+            });
+        }
+    }
+    
+    
+    }
+
