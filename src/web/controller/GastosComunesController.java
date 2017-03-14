431794fa5ffbc2ec787bf6651775d354acb3f7c7
dnacher\Daniel <danielnacher@gmail.com>
2017-03-14 16:10:08 -0300

    reportes

--- src/web/controller/GastosComunesController.java
+++ src/web/controller/GastosComunesController.java
@@ -20,7 +20,6 @@ import java.time.ZoneId;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
